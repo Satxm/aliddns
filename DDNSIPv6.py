f22a1404b02cf3e211fccaa4519fbc0297dc012e
Satxm <56530136+Satxm@users.noreply.github.com>
2020-07-18 14:21:13 +0800

    Update DDNSIPv6.py

--- DDNSIPv6.py
+++ DDNSIPv6.py
@@ -11,8 +11,8 @@ import re
 
 accessKeyId = "accessKeyId"  # 将accessKeyId改成自己的accessKeyId
 accessSecret = "accessSecret"  # 将accessSecret改成自己的accessSecret
-domain = "baidu.com"  # 你的主域名
-name_ipv6 = "ipv6"  # 要进行ipv6 ddns解析的子域名
+domain = "domain.main"  # 你的主域名
+names = ['name1','name2','name3']  # 要进行ipv6 ddns解析的子域名
 
 client = AcsClient(accessKeyId, accessSecret, 'cn-hangzhou')
 
@@ -42,31 +42,31 @@ def getIPv6Address():  # 获取本地IPv6公网地址
     return result[0][0]
 
 if __name__ == "__main__":
-    request = DescribeSubDomainRecordsRequest()
-    request.set_accept_format('json')
-    request.set_DomainName(domain)
-    request.set_SubDomain(name_ipv6 + '.' + domain)
-    response = client.do_action_with_exception(request)
-    domain_list = json.loads(response)
-
     ipv6 = getIPv6Address()
-    print("获取到IPv6地址：%s" % ipv6)
-
-    if domain_list['TotalCount'] == 0:
-        add(domain, name_ipv6, "AAAA", ipv6)
-        print("新建域名解析成功")
-    elif domain_list['TotalCount'] == 1:
-        if domain_list['DomainRecords']['Record'][0]['Value'].strip() != ipv6.strip():
-            update(domain_list['DomainRecords']['Record'][0]['RecordId'], name_ipv6, "AAAA", ipv6)
-            print("修改域名解析成功")
-        else:
-            print("IPv6地址没变")
-    elif domain_list['TotalCount'] > 1:
-        from aliyunsdkalidns.request.v20150109.DeleteSubDomainRecordsRequest import DeleteSubDomainRecordsRequest
-        request = DeleteSubDomainRecordsRequest()
+    print("获取到本机IPv6地址：%s" % ipv6)
+    for name in names:
+        request = DescribeSubDomainRecordsRequest()
         request.set_accept_format('json')
         request.set_DomainName(domain)
-        request.set_RR(name_ipv6)
+        request.set_SubDomain(name + '.' + domain)
         response = client.do_action_with_exception(request)
-        add(domain, name_ipv6, "AAAA", ipv6)
-        print("修改域名解析成功")
+        domain_list = json.loads(response)
+
+        if domain_list['TotalCount'] == 0:
+            add(domain, name, "AAAA", ipv6)
+            print("新建域名（",name + '.' + domain,"）解析成功")
+        elif domain_list['TotalCount'] == 1:
+            if domain_list['DomainRecords']['Record'][0]['Value'].strip() != ipv6.strip():
+                update(domain_list['DomainRecords']['Record'][0]['RecordId'], name, "AAAA", ipv6)
+                print("修改域名（",name + '.' + domain,"）解析成功")
+            else:
+                print("域名（",name + '.' + domain,"）IPv6地址没变")
+        elif domain_list['TotalCount'] > 1:
+            from aliyunsdkalidns.request.v20150109.DeleteSubDomainRecordsRequest import DeleteSubDomainRecordsRequest
+            request = DeleteSubDomainRecordsRequest()
+            request.set_accept_format('json')
+            request.set_DomainName(domain)
+            request.set_RR(name)
+            response = client.do_action_with_exception(request)
+            add(domain, name, "AAAA", ipv6)
+            print("修改域名（",name + '.' + domain,"）解析成功")
